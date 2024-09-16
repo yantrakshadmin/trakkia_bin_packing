b05c5cf6b48f00f60253d88d93d8cb1cabe96fe5
Sanya <sanyaaroraa2001@gmail.com>
2024-09-16 11:05:11 +0530

    changes done

--- packing/plot_algo.py
+++ packing/plot_algo.py
@@ -4,156 +4,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
 
-# def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_item, padding, user_orientations, max_weight=None):
-#     def can_place_item(x_start, y_start, z_start, dim, occupied_spaces):
-#         x_end, y_end, z_end = x_start + dim[0], y_start + dim[1], z_start + dim[2]
-
-#         if x_end > L_box or y_end > B_box or z_end > H_box:
-#             return False
-
-#         for (ox_start, oy_start, oz_start, odim) in occupied_spaces:
-#             ox_end = ox_start + odim[0]
-#             oy_end = oy_start + odim[1]
-#             oz_end = oz_start + odim[2]
-
-#             if (x_start < ox_end and x_end > ox_start and
-#                 y_start < oy_end and y_end > oy_start and
-#                 z_start < oz_end and z_end > oz_start):
-#                 return False
-#         return True
-
-#     def items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, orientations, max_weight, weight_per_item):
-#         dimensions = {
-#             'L_B_H': (L_item, B_item, H_item),
-#             'L_H_B': (L_item, H_item, B_item),
-#             'B_L_H': (B_item, L_item, H_item),
-#             'B_H_L': (B_item, H_item, L_item),
-#             'H_L_B': (H_item, L_item, B_item),
-#             'H_B_L': (H_item, B_item, L_item)
-#         }
-
-#         total_items = 0
-#         total_volume = 0
-#         item_positions = []
-#         occupied_spaces = []
-#         orientation_count = {orientation: 0 for orientation in orientations} 
-#         for orientation in orientations:
-#             dim = dimensions[orientation]
-#             num_L = int(L_box / (dim[0] + padding))
-#             num_B = int(B_box / (dim[1] + padding))
-#             num_H = int(H_box / (dim[2] + padding))
-
-#             for i in range(num_L):
-#                 for j in range(num_B):
-#                     for k in range(num_H):
-#                         if max_weight and total_items * weight_per_item >= max_weight:
-#                             return total_items, item_positions, orientation_count
-
-#                         x = i * (dim[0] + padding)
-#                         y = j * (dim[1] + padding)
-#                         z = k * (dim[2] + padding)
-
-#                         if can_place_item(x, y, z, dim, occupied_spaces):
-#                             item_positions.append((x, y, z, dim, orientation))
-#                             occupied_spaces.append((x, y, z, dim))
-#                             total_items += 1
-#                             orientation_count[orientation] += 1
-#                             total_volume += dim[0] * dim[1] * dim[2]  # Volume of the item
-#                         else:
-#                             continue
-
-#         total_weight = total_items * weight_per_item
-
-#         return total_items, total_weight, total_volume, item_positions, orientation_count
-
-#     total_items, total_weight, total_volume, item_positions, orientation_count = items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, user_orientations, max_weight, weight_per_item)
-
-#     fig = plt.figure(figsize=(10, 8))
-#     ax = fig.add_subplot(111, projection='3d')
-
-#     def create_box(ax, L_box, B_box, H_box):
-#         r = [[0, L_box], [0, B_box], [0, H_box]]
-#         vertices = [
-#             [r[0][0], r[1][0], r[2][0]],
-#             [r[0][1], r[1][0], r[2][0]],
-#             [r[0][1], r[1][1], r[2][0]],
-#             [r[0][0], r[1][1], r[2][0]],
-#             [r[0][0], r[1][0], r[2][1]],
-#             [r[0][1], r[1][0], r[2][1]],
-#             [r[0][1], r[1][1], r[2][1]],
-#             [r[0][0], r[1][1], r[2][1]]
-#         ]
-
-#         edges = [
-#             [vertices[0], vertices[1], vertices[2], vertices[3]],
-#             [vertices[4], vertices[5], vertices[6], vertices[7]],
-#             [vertices[0], vertices[1], vertices[5], vertices[4]],
-#             [vertices[2], vertices[3], vertices[7], vertices[6]],
-#             [vertices[1], vertices[2], vertices[6], vertices[5]],
-#             [vertices[4], vertices[7], vertices[3], vertices[0]]
-#         ]
-
-#         box = Poly3DCollection(edges, alpha=0.1, facecolors='white', linewidths=0.5, edgecolors='k')
-#         ax.add_collection3d(box)
-
-#     def plot_items(ax, item_positions, padding):
-#         colors = {
-#             'L_B_H': 'blue',
-#             'L_H_B': 'lightblue',
-#             'B_L_H': 'grey',
-#             'B_H_L': 'lightgreen',
-#             'H_L_B': 'red',
-#             'H_B_L': 'salmon'
-#         }
-
-#         for x, y, z, dim, orientation in item_positions:
-#             item_color = colors[orientation]
-#             item_coords = np.array([
-#                 [x, y, z],
-#                 [x + dim[0], y, z],
-#                 [x + dim[0], y + dim[1], z],
-#                 [x, y + dim[1], z],
-#                 [x, y, z + dim[2]],
-#                 [x + dim[0], y, z + dim[2]],
-#                 [x + dim[0], y + dim[1], z + dim[2]],
-#                 [x, y + dim[1], z + dim[2]]
-#             ])
-
-#             edges = [
-#                 [item_coords[0], item_coords[1], item_coords[2], item_coords[3]],
-#                 [item_coords[4], item_coords[5], item_coords[6], item_coords[7]],
-#                 [item_coords[0], item_coords[1], item_coords[5], item_coords[4]],
-#                 [item_coords[2], item_coords[3], item_coords[7], item_coords[6]],
-#                 [item_coords[1], item_coords[2], item_coords[6], item_coords[5]],
-#                 [item_coords[4], item_coords[7], item_coords[3], item_coords[0]]
-#             ]
-
-#             item = Poly3DCollection(edges, alpha=0.6, facecolors=item_color, linewidths=0.5, edgecolors='k')
-#             ax.add_collection3d(item)
-
-#     create_box(ax, L_box, B_box, H_box)
-#     plot_items(ax, item_positions, padding)
-
-#     ax.set_xlim(0, L_box)
-#     ax.set_ylim(0, B_box)
-#     ax.set_zlim(0, H_box)
-
-#     ax.set_xlabel('Length (mm)')
-#     ax.set_ylabel('Breadth (mm)')
-#     ax.set_zlabel('Height (mm)')
-
-#     ax.set_box_aspect([np.ptp([0, L_box]), np.ptp([0, B_box]), np.ptp([0, H_box])])
-
-#     buffer = BytesIO()
-#     plt.savefig(buffer, format='png')
-#     buffer.seek(0)
-#     img_str = base64.b64encode(buffer.read()).decode('utf-8')
-#     plt.close(fig)
-    
-#     return img_str, total_items, total_weight, total_volume, orientation_count
-
-
-
 def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_item, padding, user_orientations, max_weight=None):
     def can_place_item(x_start, y_start, z_start, dim, occupied_spaces):
         x_end, y_end, z_end = x_start + dim[0], y_start + dim[1], z_start + dim[2]
@@ -182,17 +32,13 @@ def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_it
             'H_B_L': (H_item, B_item, L_item)
         }
 
-        best_orientation_data = None
-        best_total_items = 0
-
-        # Loop through each orientation and compute the max number of items that can be placed
+        total_items = 0
+        total_volume = 0
+        item_positions = []
+        occupied_spaces = []
+        orientation_count = {orientation: 0 for orientation in orientations} 
         for orientation in orientations:
             dim = dimensions[orientation]
-            total_items = 0
-            item_positions = []
-            occupied_spaces = []
-            orientation_count = {orientation: 0}
-
             num_L = int(L_box / (dim[0] + padding))
             num_B = int(B_box / (dim[1] + padding))
             num_H = int(H_box / (dim[2] + padding))
@@ -201,7 +47,7 @@ def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_it
                 for j in range(num_B):
                     for k in range(num_H):
                         if max_weight and total_items * weight_per_item >= max_weight:
-                            break
+                            return total_items, item_positions, orientation_count
 
                         x = i * (dim[0] + padding)
                         y = j * (dim[1] + padding)
@@ -212,37 +58,16 @@ def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_it
                             occupied_spaces.append((x, y, z, dim))
                             total_items += 1
                             orientation_count[orientation] += 1
+                            total_volume += dim[0] * dim[1] * dim[2]  # Volume of the item
+                        else:
+                            continue
 
-            total_volume = total_items * dim[0] * dim[1] * dim[2]
-            total_weight = total_items * weight_per_item
+        total_weight = total_items * weight_per_item
 
-            # Track the best orientation in terms of total items placed
-            if total_items > best_total_items:
-                best_total_items = total_items
-                best_orientation_data = {
-                    'orientation': orientation,
-                    'total_items': total_items,
-                    'total_weight': total_weight,
-                    'total_volume': total_volume,
-                    'item_positions': item_positions,
-                    'orientation_count': orientation_count
-                }
+        return total_items, total_weight, total_volume, item_positions, orientation_count
 
-        return best_orientation_data
+    total_items, total_weight, total_volume, item_positions, orientation_count = items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, user_orientations, max_weight, weight_per_item)
 
-    # Get the best orientation based on item placement
-    best_orientation_data = items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, user_orientations, max_weight, weight_per_item)
-
-    if not best_orientation_data:
-        return "No items could be placed", 0, 0, 0, {}
-
-    item_positions = best_orientation_data['item_positions']
-    total_items = best_orientation_data['total_items']
-    total_weight = best_orientation_data['total_weight']
-    total_volume = best_orientation_data['total_volume']
-    orientation_count = best_orientation_data['orientation_count']
-
-    # Plotting
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
 
@@ -326,6 +151,181 @@ def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_it
     plt.close(fig)
     
     return img_str, total_items, total_weight, total_volume, orientation_count
+
+
+
+# def plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_item, padding, user_orientations, max_weight=None):
+#     def can_place_item(x_start, y_start, z_start, dim, occupied_spaces):
+#         x_end, y_end, z_end = x_start + dim[0], y_start + dim[1], z_start + dim[2]
+
+#         if x_end > L_box or y_end > B_box or z_end > H_box:
+#             return False
+
+#         for (ox_start, oy_start, oz_start, odim) in occupied_spaces:
+#             ox_end = ox_start + odim[0]
+#             oy_end = oy_start + odim[1]
+#             oz_end = oz_start + odim[2]
+
+#             if (x_start < ox_end and x_end > ox_start and
+#                 y_start < oy_end and y_end > oy_start and
+#                 z_start < oz_end and z_end > oz_start):
+#                 return False
+#         return True
+
+#     def items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, orientations, max_weight, weight_per_item):
+#         dimensions = {
+#             'L_B_H': (L_item, B_item, H_item),
+#             'L_H_B': (L_item, H_item, B_item),
+#             'B_L_H': (B_item, L_item, H_item),
+#             'B_H_L': (B_item, H_item, L_item),
+#             'H_L_B': (H_item, L_item, B_item),
+#             'H_B_L': (H_item, B_item, L_item)
+#         }
+
+#         best_orientation_data = None
+#         best_total_items = 0
+
+#         # Loop through each orientation and compute the max number of items that can be placed
+#         for orientation in orientations:
+#             dim = dimensions[orientation]
+#             total_items = 0
+#             item_positions = []
+#             occupied_spaces = []
+#             orientation_count = {orientation: 0}
+
+#             num_L = int(L_box / (dim[0] + padding))
+#             num_B = int(B_box / (dim[1] + padding))
+#             num_H = int(H_box / (dim[2] + padding))
+
+#             for i in range(num_L):
+#                 for j in range(num_B):
+#                     for k in range(num_H):
+#                         if max_weight and total_items * weight_per_item >= max_weight:
+#                             break
+
+#                         x = i * (dim[0] + padding)
+#                         y = j * (dim[1] + padding)
+#                         z = k * (dim[2] + padding)
+
+#                         if can_place_item(x, y, z, dim, occupied_spaces):
+#                             item_positions.append((x, y, z, dim, orientation))
+#                             occupied_spaces.append((x, y, z, dim))
+#                             total_items += 1
+#                             orientation_count[orientation] += 1
+
+#             total_volume = total_items * dim[0] * dim[1] * dim[2]
+#             total_weight = total_items * weight_per_item
+
+#             # Track the best orientation in terms of total items placed
+#             if total_items > best_total_items:
+#                 best_total_items = total_items
+#                 best_orientation_data = {
+#                     'orientation': orientation,
+#                     'total_items': total_items,
+#                     'total_weight': total_weight,
+#                     'total_volume': total_volume,
+#                     'item_positions': item_positions,
+#                     'orientation_count': orientation_count
+#                 }
+
+#         return best_orientation_data
+
+#     # Get the best orientation based on item placement
+#     best_orientation_data = items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, padding, user_orientations, max_weight, weight_per_item)
+
+#     if not best_orientation_data:
+#         return "No items could be placed", 0, 0, 0, {}
+
+#     item_positions = best_orientation_data['item_positions']
+#     total_items = best_orientation_data['total_items']
+#     total_weight = best_orientation_data['total_weight']
+#     total_volume = best_orientation_data['total_volume']
+#     orientation_count = best_orientation_data['orientation_count']
+
+#     # Plotting
+#     fig = plt.figure(figsize=(10, 8))
+#     ax = fig.add_subplot(111, projection='3d')
+
+#     def create_box(ax, L_box, B_box, H_box):
+#         r = [[0, L_box], [0, B_box], [0, H_box]]
+#         vertices = [
+#             [r[0][0], r[1][0], r[2][0]],
+#             [r[0][1], r[1][0], r[2][0]],
+#             [r[0][1], r[1][1], r[2][0]],
+#             [r[0][0], r[1][1], r[2][0]],
+#             [r[0][0], r[1][0], r[2][1]],
+#             [r[0][1], r[1][0], r[2][1]],
+#             [r[0][1], r[1][1], r[2][1]],
+#             [r[0][0], r[1][1], r[2][1]]
+#         ]
+
+#         edges = [
+#             [vertices[0], vertices[1], vertices[2], vertices[3]],
+#             [vertices[4], vertices[5], vertices[6], vertices[7]],
+#             [vertices[0], vertices[1], vertices[5], vertices[4]],
+#             [vertices[2], vertices[3], vertices[7], vertices[6]],
+#             [vertices[1], vertices[2], vertices[6], vertices[5]],
+#             [vertices[4], vertices[7], vertices[3], vertices[0]]
+#         ]
+
+#         box = Poly3DCollection(edges, alpha=0.1, facecolors='white', linewidths=0.5, edgecolors='k')
+#         ax.add_collection3d(box)
+
+#     def plot_items(ax, item_positions, padding):
+#         colors = {
+#             'L_B_H': 'blue',
+#             'L_H_B': 'lightblue',
+#             'B_L_H': 'grey',
+#             'B_H_L': 'lightgreen',
+#             'H_L_B': 'red',
+#             'H_B_L': 'salmon'
+#         }
+
+#         for x, y, z, dim, orientation in item_positions:
+#             item_color = colors[orientation]
+#             item_coords = np.array([
+#                 [x, y, z],
+#                 [x + dim[0], y, z],
+#                 [x + dim[0], y + dim[1], z],
+#                 [x, y + dim[1], z],
+#                 [x, y, z + dim[2]],
+#                 [x + dim[0], y, z + dim[2]],
+#                 [x + dim[0], y + dim[1], z + dim[2]],
+#                 [x, y + dim[1], z + dim[2]]
+#             ])
+
+#             edges = [
+#                 [item_coords[0], item_coords[1], item_coords[2], item_coords[3]],
+#                 [item_coords[4], item_coords[5], item_coords[6], item_coords[7]],
+#                 [item_coords[0], item_coords[1], item_coords[5], item_coords[4]],
+#                 [item_coords[2], item_coords[3], item_coords[7], item_coords[6]],
+#                 [item_coords[1], item_coords[2], item_coords[6], item_coords[5]],
+#                 [item_coords[4], item_coords[7], item_coords[3], item_coords[0]]
+#             ]
+
+#             item = Poly3DCollection(edges, alpha=0.6, facecolors=item_color, linewidths=0.5, edgecolors='k')
+#             ax.add_collection3d(item)
+
+#     create_box(ax, L_box, B_box, H_box)
+#     plot_items(ax, item_positions, padding)
+
+#     ax.set_xlim(0, L_box)
+#     ax.set_ylim(0, B_box)
+#     ax.set_zlim(0, H_box)
+
+#     ax.set_xlabel('Length (mm)')
+#     ax.set_ylabel('Breadth (mm)')
+#     ax.set_zlabel('Height (mm)')
+
+#     ax.set_box_aspect([np.ptp([0, L_box]), np.ptp([0, B_box]), np.ptp([0, H_box])])
+
+#     buffer = BytesIO()
+#     plt.savefig(buffer, format='png')
+#     buffer.seek(0)
+#     img_str = base64.b64encode(buffer.read()).decode('utf-8')
+#     plt.close(fig)
+    
+#     return img_str, total_items, total_weight, total_volume, orientation_count
 
 
 CONTAINER_TRUCK_DIMENSIONS = {
--- packing/views.py
+++ packing/views.py
@@ -10,39 +10,89 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
 
-def pack_items_view(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            L_item = form.cleaned_data['length']
-            B_item = form.cleaned_data['breadth']
-            H_item = form.cleaned_data['height']
-            box_key = form.cleaned_data['truck_type']
-            padding = form.cleaned_data['padding']
-            weight_per_item = form.cleaned_data['weight']
-            user_orientations = form.cleaned_data['orientations']
+# def pack_items_view(request):
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST)
+#         if form.is_valid():
+#             L_item = form.cleaned_data['length']
+#             B_item = form.cleaned_data['breadth']
+#             H_item = form.cleaned_data['height']
+#             box_key = form.cleaned_data['truck_type']
+#             padding = form.cleaned_data['padding']
+#             weight_per_item = form.cleaned_data['weight']
+#             user_orientations = form.cleaned_data['orientations']
 
-            box_dimensions = get_box_dimensions(box_key)
-            L_box = box_dimensions['L_box']
-            B_box = box_dimensions['B_box']
-            H_box = box_dimensions['H_box']
-            max_weight = box_dimensions['max_weight']
+#             box_dimensions = get_box_dimensions(box_key)
+#             L_box = box_dimensions['L_box']
+#             B_box = box_dimensions['B_box']
+#             H_box = box_dimensions['H_box']
+#             max_weight = box_dimensions['max_weight']
 
-            image_path, total_items, total_weight, total_volume, orientation_count = plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_item=weight_per_item, padding=padding, user_orientations=user_orientations, max_weight=max_weight)
+#             image_path, total_items, total_weight, total_volume, orientation_count = plot_items_in_box(L_box, B_box, H_box, L_item, B_item, H_item, weight_per_item=weight_per_item, padding=padding, user_orientations=user_orientations, max_weight=max_weight)
 
-            image_file = ContentFile(base64.b64decode(image_path), name=f'{box_key}_plot.png')
-            image_path = default_storage.save(f'plots/{box_key}_plot.png', image_file)
-            image_url = f"{settings.MEDIA_URL}{image_path}"
+#             image_file = ContentFile(base64.b64decode(image_path), name=f'{box_key}_plot.png')
+#             image_path = default_storage.save(f'plots/{box_key}_plot.png', image_file)
+#             image_url = f"{settings.MEDIA_URL}{image_path}"
             
-            context = {
-                'total_items': total_items,
-                'orientation_count': orientation_count,
-                'total_weight': round(total_weight, 2),
-                'total_volume': round(total_volume/1000, 2),
-                'image_path': image_url,
-            }
-            return render(request, 'packing/result.html', context)
-    else:
-        form = ItemForm()
+#             context = {
+#                 'total_items': total_items,
+#                 'orientation_count': orientation_count,
+#                 'total_weight': round(total_weight, 2),
+#                 'total_volume': round(total_volume/1000, 2),
+#                 'image_path': image_url,
+#             }
+#             return render(request, 'packing/result.html', context)
+#     else:
+#         form = ItemForm()
 
-    return render(request, 'packing/packing_form.html', {'form': form})
+#     return render(request, 'packing/packing_form.html', {'form': form})
+
+
+
+from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.conf import settings
+import base64
+
+def pack_items_view(request):
+    form = ItemForm(request.POST or None)
+    context = {}
+
+    if request.method == 'POST' and form.is_valid():
+        L_item = form.cleaned_data['length']
+        B_item = form.cleaned_data['breadth']
+        H_item = form.cleaned_data['height']
+        box_key = form.cleaned_data['truck_type']
+        padding = form.cleaned_data['padding']
+        weight_per_item = form.cleaned_data['weight']
+        user_orientations = form.cleaned_data['orientations']
+
+        box_dimensions = get_box_dimensions(box_key)
+        L_box = box_dimensions['L_box']
+        B_box = box_dimensions['B_box']
+        H_box = box_dimensions['H_box']
+        max_weight = box_dimensions['max_weight']
+
+        image_path, total_items, total_weight, total_volume, orientation_count = plot_items_in_box(
+            L_box, B_box, H_box, L_item, B_item, H_item,
+            weight_per_item=weight_per_item,
+            padding=padding,
+            user_orientations=user_orientations,
+            max_weight=max_weight
+        )
+
+        image_file = ContentFile(base64.b64decode(image_path), name=f'{box_key}_plot.png')
+        image_path = default_storage.save(f'plots/{box_key}_plot.png', image_file)
+        image_url = f"{settings.MEDIA_URL}{image_path}"
+
+        context.update({
+            'total_items': total_items,
+            'orientation_count': orientation_count,
+            'total_weight': round(total_weight, 2),
+            'total_volume': round(total_volume / 1000, 2),
+            'image_path': image_url,
+        })
+
+    context['form'] = form
+    return render(request, 'packing/packing_form.html', context)
